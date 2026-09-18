6d7daf290f127ecacbb6b2e4709c5120203ff06f
SeniorLePenguin <188710659+SeniorLePenguin@users.noreply.github.com>
2026-09-19 00:52:11 +0300

    Added a function to sum the values of rolled dice
    The function uses the rollDice function, then sums up all the rolled values.
    Returns a tuple with the total, and the list of rolls for the sake of clarity.

--- dice.py
+++ dice.py
@@ -6,3 +6,9 @@ def rollDice(number, sides):
     result = [random.randrange(1, sides + 1) for x in range(number)]
     return result
 
+
+# Returns the sum of all values, as well as the rolls (Maybe add a fixed value bonus as well, though I prefer to keep it basic for now)
+def diceSummer(number, sides):
+    rolls = rollDice(number, sides)
+    total = sum( rollDice(number, sides) )
+    return (total, rolls)
