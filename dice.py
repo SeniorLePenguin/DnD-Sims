302710ae8a60744c8c503da76832e0f6dfb5f15e
SeniorLePenguin <188710659+SeniorLePenguin@users.noreply.github.com>
2026-09-20 12:00:36 +0300

    Added a general d20 check function
    Added a function for conducting a general d20 check roll.
    It includes disadvantage, modifiers, and proficiency bonuses.
    Returns the final result. To be used as a skeleton for other tests
    like attack rolls.

--- dice.py
+++ dice.py
@@ -12,3 +12,22 @@ def diceSummer(number, sides):
     rolls = rollDice(number, sides)
     total = sum( rollDice(number, sides) )
     return (total, rolls)
+
+
+# Returns the value of a general d20 check
+def d20Check(mod = 0, prof = 0, adv = None):
+
+    #Checks if we have adv/disadv (corresponds to True/False), and makes the relevant rolls.
+    if adv is None:
+        roll = rollDice()[0]
+    else:
+        rolls = rollDice(20,2)
+
+        if adv:
+            roll = max(rolls)
+        else:
+            roll = min(rolls)
+
+    result = roll + mod + prof  #Adds the relevant ability modifier and proficiency (if applicable)
+    return result    
+
