e53d8a18fccb37d22e3360e3b59abeb16d66cf71
SeniorLePenguin <188710659+SeniorLePenguin@users.noreply.github.com>
2026-09-20 11:19:06 +0300

    Added default values for rollDice arguments
    Added default number of sides and dice rolled for quick calls.
    Also changed the order, since we will often roll a single die,
    this way we can specify only the sides.

--- dice.py
+++ dice.py
@@ -2,7 +2,7 @@ import random
 
 
 # Returns a list of values of 'number' rolled dice with the given sides
-def rollDice(number, sides):
+def rollDice(sides = 20, number = 1):
     result = [random.randrange(1, sides + 1) for x in range(number)]
     return result
 
